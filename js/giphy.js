c17305d6eafa38de59377a662a8ba3f09207fe59
Becky Beauchamp <beckybeauchamp1@gmail.com>
2015-11-28 15:05:14 -0500

    Edit makeGif function

--- js/giphy.js
+++ js/giphy.js
@@ -2,36 +2,23 @@ $(document).ready(function(){
 
   $("button").click(function(evt){
     evt.preventDefault();
-    var api = "http://api.giphy.com/v1/gifs/search?";
-    var apiKey = "&api_key=dc6zaTOxFJmzC";
+    var api = "http://api.giphy.com/v1/gifs/search?&api_key=dc6zaTOxFJmzC";
     var search = $("#keyword").val();
     var q = "&q=" + search;
-    var url = api + apiKey + q;
-    var i = 0;
+    var url = api + q;
     var count = 0;
+    var i = 0;
     var totalgifs;
-
-    $.ajax({
-      url: url,
-      dataType: "json",
-      type: "get",
-    }).done(function(response){
-      console.log("this worked!");
-      totalgifs = response.data.length;
-    }).fail(function(){
-      console.log("this failed");
-    }).always(function(){
-      console.log("This will always work!");
-    });
 
     makeGifs();
 
     function makeGifs(){
-      $.get(url, function(response) {
+      $.getJSON(url, function(response){
+        totalgifs = response.data.length;
         for(i; i < totalgifs; i++){
-          console.log(i);
-          if(count < 10){
+          if (count < 10){
             count++;
+            console.log(i);
             $("body").append("<div class = 'gifs'><img src='"+ response.data[i].images.original.url + "'></div>");
           }
           else{
@@ -39,8 +26,9 @@ $(document).ready(function(){
             break;
           }
         }
-      }, "json" );
+      });
     }
+
 
     $(window).scroll(function(){
       if($(window).scrollTop() + $(window).height() > $(document).height() - 100){
@@ -52,5 +40,4 @@ $(document).ready(function(){
       }
     });
   });
-
 });
