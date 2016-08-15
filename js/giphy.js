54435c685857f7a548c0d519f42d53fff818be74
Rasheeda Alexander <sheedabeeda@gmail.com>
2016-08-15 18:28:05 -0400

    Added a counter to display different sets of s of 10 images whenever the user scrolled to the bottom of the page :)

--- js/giphy.js
+++ js/giphy.js
@@ -1,9 +1,12 @@
+var limit = 100;
+var offset = 100;
+var counter = 0;
+
 $(document).ready(function(){
   $("button").on('click', function(evt){
     evt.preventDefault();
 
-    var limit = 100;
-    var offset = 100;
+
 
     var url = "http://api.giphy.com/v1/gifs/search?q=" + $('.search-input').val() + "&limit=" + limit + "&offset=" + offset + "&api_key=dc6zaTOxFJmzC"
     // var url = "http://api.giphy.com/v1/gifs/search?q=" + $('.search-input').val() + "&api_key=dc6zaTOxFJmzC"
@@ -13,24 +16,29 @@ $(document).ready(function(){
       dataType: "json"
     }).done(function(response){
       console.log(response.data[0].url);
-      function tenPics(){
-        for(i=0; i<=9; i++){
-          $('body').append('<img src="'+response.data[i].images.fixed_height.url+'">');
-            $(window).scroll(function() {
-              if($(window).scrollTop() + $(window).height() == $(document).height()) {
-                for(j=11; j<=20; j++){
-                console.log("more coming!");
-                $('body').append('<img src="'+response.data[j].images.fixed_height.url+'">');
-              }
-            };
-          });
-        };
-      }
-      tenPics();
+
+      tenPics(response);
     }).fail(function(){
       console.log("Well this sux. It failed :(");
     }).always(function(){
       console.log("This always happens");
     })
   })
+
+  function tenPics(response){
+    for(i=0; i<=9; i++){
+      $('body').append('<img src="'+response.data[10 * counter + i].images.fixed_height.url+'">');
+      $(window).scroll(function() {
+        if($(window).scrollTop() + $(window).height() == $(document).height()) {
+          counter++;
+          console.log(counter);
+          for(j=11; j<=20; j++){
+            console.log("more coming!");
+            $('body').append('<img src="'+response.data[10 * counter + j].images.fixed_height.url+'">');
+          }
+        };
+      });
+    };
+    console.log(counter++);
+  }
 })
