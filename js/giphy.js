a92cbae574e607d77804f5d1ecc30821d7ef888a
Megan Coyle <meg@mcoyle.com>
2016-08-15 13:33:01 -0400

    Set up new url with variable

--- js/giphy.js
+++ js/giphy.js
@@ -1,6 +1,18 @@
 $( document ).ready(function() {
     var $form = $("#search");
-    var $keyword = $("#keyword");
+    var $keyword = $("#keyword").val();
 
-    
+    $form.on("submit", function(e) {
+      e.preventDefault();
+    $.ajax({
+      type: "get",
+      dataType: "json",
+      url: "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC"
+    }).done(function(res){
+      console.log(res);
+    }).fail(function(res) {
+      console.log("you have failed me for the last time");
+    })
+  })
+
 });
