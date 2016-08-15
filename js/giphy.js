2b9345d231ac24de7c449790f4f62f08e994b3dd
Megan Coyle <meg@mcoyle.com>
2016-08-15 16:07:18 -0400

    Attempt to add infinite scroll

--- js/giphy.js
+++ js/giphy.js
@@ -23,23 +23,49 @@ $( document ).ready(function() {
       for (var i = 0; i < l; i++) {
           images[0].parentNode.removeChild(images[0]);
       }
+
       // grab keyword value
       var $keyword = $("#keyword").val();
-      
+      var offset = 0;
+
     $.ajax({
       type: "get",
       dataType: "json",
-      url: "http://api.giphy.com/v1/gifs/search?q=" + $keyword + "&api_key=dc6zaTOxFJmzC"
-    }).done(function(res){
+      url: "http://api.giphy.com/v1/gifs/search?q=" + $keyword + "&offset=" + offset + "&api_key=dc6zaTOxFJmzC"
+      }).done(function(res){
       for (i = 0; i < res.data.length; i++) {
           var newImg = "<img src=" + res.data[i].images.fixed_height.url + ">";
           $("body").append(newImg);
         };
+
+        // scroll function
+        $(window).on('scroll', function() {
+          if($(window).scrollTop() + $(window).height() >= $(document).height()){
+            offset++;
+            $.ajax({
+              type: "get",
+              dataType: "json",
+              url: "http://api.giphy.com/v1/gifs/search?q=" + $keyword + "&offset=" + offset + "&api_key=dc6zaTOxFJmzC"
+            }).done(function(res){
+              for (i = 0; i < res.data.length; i++) {
+                  var newImg = "<img src=" + res.data[i].images.fixed_height.url + ">";
+                  $("body").append(newImg);
+                };
+              });
+
+          }
+        });
+
+        // toggle image class
+        $("img").on("click", function(){
+          $(this).toggleClass('big');
+        })
+
         // clear input
-        $("#keyword").val('');
-    }).fail(function(res) {
-      console.log("oh nos, an error!");
-    })
+            $("#keyword").val('');
+        }).fail(function(res) {
+          console.log("oh nos, an error!");
+        })
   })
 
 });
