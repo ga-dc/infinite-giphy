a2a4e05c33884bd6f76b8611db27e476502ccee1
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-08-15 17:01:22 -0400

    got 10 gifs to display on page

---
+++ js/giphy.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $("button").on("click", function(evt){
+    evt.preventDefault()
+    var keyword = $("#keyword").val()
+    var limit = 100
+    $.ajax({
+      type: "GET",
+      dataType: "json",
+      url: "http://api.giphy.com/v1/gifs/search?q="+keyword+"&limit="+limit+"&api_key=dc6zaTOxFJmzC"
+    }).done(function(response){
+      for (i=0; i<10; i++){
+        // console.log(response.data[i].url)
+        $(".giphy").append('<div><img src="'+response.data[i].images.fixed_height.url+'" /></div>');
+      }
+    }).fail(function(response){
+    }).always(function(){
+    })
+  })
+})
