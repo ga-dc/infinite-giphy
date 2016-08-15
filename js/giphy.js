133eaddede033408f3783a5b1036758ca2fa85b4
Megan Coyle <meg@mcoyle.com>
2016-08-15 14:07:12 -0400

    Got giphys to show up - attempting to do constructor

--- js/giphy.js
+++ js/giphy.js
@@ -1,17 +1,34 @@
 $( document ).ready(function() {
     var $form = $("#search");
-    var $keyword = $("#keyword").val();
+
+    // Gify constructor
+    // function Gify(options) {
+    //   var options = options.data
+    //   this.image = options.data.images.fixed_height.url
+    // }
+    //
+    // Gify.protoype = {
+    //   render: function() {
+    //     var container = $("<div></div>")
+    //     container.append($("<img src=" +  + "/>"))
+    //   }
+    // }
+
 
     $form.on("submit", function(e) {
       e.preventDefault();
+      var $keyword = $("#keyword").val();
     $.ajax({
       type: "get",
       dataType: "json",
-      url: "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC"
+      url: "http://api.giphy.com/v1/gifs/search?q=" + $keyword + "&api_key=dc6zaTOxFJmzC"
     }).done(function(res){
-      console.log(res);
+      for (i = 0; i < res.data.length; i++) {
+          var newImg = "<img src=" + res.data[i].images.fixed_height.url + ">";
+          $("body").append(newImg);
+        };
     }).fail(function(res) {
-      console.log("you have failed me for the last time");
+      console.log("oh nos, an error!");
     })
   })
 
