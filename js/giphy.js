21e8090472b4f7a8afafd53e54b8f3101f88b040
Christopher Phillips <cphillips918@gmail.com>
2016-08-15 20:11:27 -0400

    got the infitinite scroll to work. now going onto the problem of the same 10 giphs loading

--- js/giphy.js
+++ js/giphy.js
@@ -3,7 +3,7 @@ $('form').submit(function(event) {
     console.log('Iwork')
     var search = $('input').val()
     console.log(search)
-    var url = "http://api.giphy.com/v1/gifs/search?q="+search+"&api_key=dc6zaTOxFJmzC"
+    var url = "http://api.giphy.com/v1/gifs/search?q="+search+"&api_key=dc6zaTOxFJmzC&limit=10"
     $.ajax({
         url: url,
         type: 'get',
@@ -21,4 +21,28 @@ $('form').submit(function(event) {
         console.log("complete");
     });
 
+});
+
+$(window).scroll(function(event) {
+    if($(window).scrollTop() + $(window).height() === $(document).height()){
+        var search = $('input').val()
+        var url = "http://api.giphy.com/v1/gifs/search?q="+search+"&api_key=dc6zaTOxFJmzC&limit=10"
+        $.ajax({
+            url: url,
+            type: 'get',
+            dataType: 'json'
+        })
+        .done(function(response) {
+            console.log("success");
+            var giph = new Giph(response)
+            var giphView = new GiphView(giph)
+        })
+        .fail(function() {
+            console.log("error");
+        })
+        .always(function() {
+            console.log("complete");
+        });
+
+    }
 });
